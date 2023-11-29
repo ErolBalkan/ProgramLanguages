4543ec792cdf51ce2d311548ec0863653867b290
ErolBalkan <148368147+ErolBalkan@users.noreply.github.com>
2023-11-29 18:47:39 +0100

    Create Demo.java

---
+++ Persisting Objects with Serialization/Demo.java
@@ -0,0 +1,13 @@
+
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Student student = new Student("Erol","075400576",1005,9999);
+        StudentFileStream fileStream = new StudentFileStream();
+        String fileName = "student.txt";
+        fileStream.writeToFile(student, fileName);
+        fileStream.readFromFile(fileName);
+    }
+
+}
